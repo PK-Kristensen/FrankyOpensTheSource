f61d67ff4aefff7aae272457383da58e645cb647
KrissKristensen <108392819+KrissKristensen@users.noreply.github.com>
2023-04-13 16:22:29 +0200

    new dashboard page

---
+++ pages/Dashboard.js
@@ -0,0 +1,23 @@
+// pages/Dashboard.js
+import Link from 'next/link';
+import styles from '@/styles/Dashboard.module.css';
+
+export default function Dashboard() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Dashboard</h1>
+      <div className={styles.buttons}>
+        <Link href="/find-a-job">
+          <a>
+            <button className={styles.button}>Find a Job</button>
+          </a>
+        </Link>
+        <Link href="/create-a-job">
+          <a>
+            <button className={styles.button}>Create a Job</button>
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+}
--- pages/components/LoginWithMetamask.js
+++ pages/components/LoginWithMetamask.js
@@ -1,7 +1,9 @@
+// components/LoginWithMetamask.js
 import React, { useState, useEffect } from 'react';
 import Web3 from 'web3';
 import BlockchainButton from './BlockchainButton';
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
 
 const AccountText = styled.p`
   font-size: 16px;
@@ -10,43 +12,46 @@ const AccountText = styled.p`
 `;
 
 const LoginWithMetamask = () => {
-    const [account, setAccount] = useState('');
-    const [web3, setWeb3] = useState(null);
+  const [account, setAccount] = useState('');
+  const [web3, setWeb3] = useState(null);
+  const router = useRouter();
 
-    useEffect(() => {
-        if (window.ethereum) {
-            const web3Instance = new Web3(window.ethereum);
-            setWeb3(web3Instance);
-        } else {
-            alert('Please install MetaMask to use this feature.');
-        }
-    }, []);
+  useEffect(() => {
+    if (window.ethereum) {
+      const web3Instance = new Web3(window.ethereum);
+      setWeb3(web3Instance);
+    } else {
+      alert('Please install MetaMask to use this feature.');
+    }
+  }, []);
 
-    const connectToMetamask = async () => {
-        if (!web3) {
-            alert('MetaMask not found. Please install the extension.');
-            return;
-        }
+  const connectToMetamask = async () => {
+    if (!web3) {
+      alert('MetaMask not found. Please install the extension.');
+      return;
+    }
 
-        try {
-            const accounts = await web3.eth.requestAccounts();
-            setAccount(accounts[0]);
-        } catch (err) {
-            alert('Failed to connect to MetaMask. Please try again.');
-        }
-    };
+    try {
+      const accounts = await web3.eth.requestAccounts();
+      setAccount(accounts[0]);
+      router.push('/Dashboard');
+    } catch (err) {
+      alert('Failed to connect to MetaMask. Please try again.');
+    }
+  };
 
-    return (
-        <div>
-            <AccountText>
-                {account
-                    ? `Connected to MetaMask with account: ${account}`
-                    : 'Connect to MetaMask to continue'}
-            </AccountText>
-            {!account && (
-                <BlockchainButton onClick={connectToMetamask}>Login with MetaMask</BlockchainButton>
-            )}
-        </div>
-    );
-}
+  return (
+    <div>
+      <AccountText>
+        {account
+          ? `Connected to MetaMask with account: ${account}`
+          : 'Connect to MetaMask to continue'}
+      </AccountText>
+      {!account && (
+        <BlockchainButton onClick={connectToMetamask}>Login with MetaMask</BlockchainButton>
+      )}
+    </div>
+  );
+};
+
 export default LoginWithMetamask;
