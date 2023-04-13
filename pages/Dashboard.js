20d582a6c2c399515885f6e8aefaac5a8a06f5bf
KrissKristensen <108392819+KrissKristensen@users.noreply.github.com>
2023-04-13 16:24:08 +0200

    fjernet title

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -5,7 +5,6 @@ import styles from '@/styles/Dashboard.module.css';
 export default function Dashboard() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Dashboard</h1>
       <div className={styles.buttons}>
         <Link href="/find-a-job">
           <a>
