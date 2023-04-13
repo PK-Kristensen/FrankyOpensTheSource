b914f64b55e1cec837d1c633bda579bbaa6b6fb9
KrissKristensen <108392819+KrissKristensen@users.noreply.github.com>
2023-04-13 16:00:41 +0200

    blockchain button update

--- pages/components/BlockchainButton.js
+++ pages/components/BlockchainButton.js
@@ -1,33 +1,20 @@
-import React, { useState } from 'react';
-import Web3 from 'web3';
-import BlockchainButton from './BlockchainButton';
 import styled from 'styled-components';
 
-const AccountText = styled.p`
+const BlockchainButton = styled.button`
+  background: linear-gradient(45deg, #f3ec78, #af4261);
+  border: none;
+  border-radius: 50px;
+  color: white;
+  cursor: pointer;
   font-size: 16px;
   font-weight: 600;
-  margin: 16px 0;
+  padding: 12px 24px;
+  text-transform: uppercase;
+  transition: 0.2s ease-in-out;
+
+  &:hover {
+    background: linear-gradient(45deg, #af4261, #f3ec78);
+  }
 `;
 
-const LoginWithMetaMask = () => {
-  const [account, setAccount] = useState('');
-
-  const connectToMetamask = async () => {
-    // ... (the rest of the connectToMetamask function)
-  };
-
-  return (
-    <div>
-      <AccountText>
-        {account
-          ? `Connected to MetaMask with account: ${account}`
-          : 'Connect to MetaMask to continue'}
-      </AccountText>
-      {!account && (
-        <BlockchainButton onClick={connectToMetamask}>Login with MetaMask</BlockchainButton>
-      )}
-    </div>
-  );
-};
-
-export default LoginWithMetaMask;
+export default BlockchainButton;
--- pages/components/LoginWithMetamask.js
+++ pages/components/LoginWithMetamask.js
@@ -1,20 +1,52 @@
+import React, { useState } from 'react';
+import Web3 from 'web3';
+import BlockchainButton from './BlockchainButton';
 import styled from 'styled-components';
 
-const BlockchainButton = styled.button`
-  background: linear-gradient(45deg, #f3ec78, #af4261);
-  border: none;
-  border-radius: 50px;
-  color: white;
-  cursor: pointer;
+const AccountText = styled.p`
   font-size: 16px;
   font-weight: 600;
-  padding: 12px 24px;
-  text-transform: uppercase;
-  transition: 0.2s ease-in-out;
-
-  &:hover {
-    background: linear-gradient(45deg, #af4261, #f3ec78);
-  }
+  margin: 16px 0;
 `;
 
-export default BlockchainButton;
+const LoginWithMetamask = () => {
+    const [account, setAccount] = useState('');
+    const [web3, setWeb3] = useState(null);
+
+    useEffect(() => {
+        if (window.ethereum) {
+            const web3Instance = new Web3(window.ethereum);
+            setWeb3(web3Instance);
+        } else {
+            alert('Please install MetaMask to use this feature.');
+        }
+    }, []);
+
+    const connectToMetamask = async () => {
+        if (!web3) {
+            alert('MetaMask not found. Please install the extension.');
+            return;
+        }
+
+        try {
+            const accounts = await web3.eth.requestAccounts();
+            setAccount(accounts[0]);
+        } catch (err) {
+            alert('Failed to connect to MetaMask. Please try again.');
+        }
+    };
+
+    return (
+        <div>
+            <AccountText>
+                {account
+                    ? `Connected to MetaMask with account: ${account}`
+                    : 'Connect to MetaMask to continue'}
+            </AccountText>
+            {!account && (
+                <BlockchainButton onClick={connectToMetamask}>Login with MetaMask</BlockchainButton>
+            )}
+        </div>
+    );
+}
+export default LoginWithMetamask;
