459d1da2d6de15754ac9a31c4f2dd13a550812a3
KrissKristensen <108392819+KrissKristensen@users.noreply.github.com>
2023-04-13 15:55:01 +0200

    Update LoginWithMetamask.js

--- pages/components/LoginWithMetamask.js
+++ pages/components/LoginWithMetamask.js
@@ -10,43 +10,43 @@ const AccountText = styled.p`
 `;
 
 const LoginWithMetamask = () => {
-  const [account, setAccount] = useState('');
-  const [web3, setWeb3] = useState(null);
+    const [account, setAccount] = useState('');
+    const [web3, setWeb3] = useState(null);
 
-  useEffect(() => {
-    if (window.ethereum) {
-      const web3Instance = new Web3(window.ethereum);
-      setWeb3(web3Instance);
-    } else {
-      alert('Please install MetaMask to use this feature.');
-    }
-  }, []);
+    useEffect(() => {
+        if (window.ethereum) {
+            const web3Instance = new Web3(window.ethereum);
+            setWeb3(web3Instance);
+        } else {
+            alert('Please install MetaMask to use this feature.');
+        }
+    }, []);
 
-  const connectToMetamask = async () => {
-    if (!web3) {
-      alert('MetaMask not found. Please install the extension.');
-      return;
-    }
+    const connectToMetamask = async () => {
+        if (!web3) {
+            alert('MetaMask not found. Please install the extension.');
+            return;
+        }
 
-    try {
-      const accounts = await web3.eth.requestAccounts();
-      setAccount(accounts[0]);
-    } catch (err) {
-      alert('Failed to connect to MetaMask. Please try again.');
-    }
-  };
+        try {
+            const accounts = await web3.eth.requestAccounts();
+            setAccount(accounts[0]);
+        } catch (err) {
+            alert('Failed to connect to MetaMask. Please try again.');
+        }
+    };
 
-  return (
-    <div>
-      <AccountText>
-        {account
-          ? `Connected to MetaMask with account: ${account}`
-          : 'Connect to MetaMask to continue'}
-      </AccountText>
-      {!account && (
-        <BlockchainButton onClick={connectToMetamask}>Login with MetaMask</BlockchainButton>
-      )}
-    </div>
-  );
-
+    return (
+        <div>
+            <AccountText>
+                {account
+                    ? `Connected to MetaMask with account: ${account}`
+                    : 'Connect to MetaMask to continue'}
+            </AccountText>
+            {!account && (
+                <BlockchainButton onClick={connectToMetamask}>Login with MetaMask</BlockchainButton>
+            )}
+        </div>
+    );
+}
 export default LoginWithMetamask;
